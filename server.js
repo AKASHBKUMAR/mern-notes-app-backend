84c21bb442ddc6c3603f2d3edee728553d0f11cd
Akash Kumar B <akashbkumar30@gmail.com>
2024-10-22 06:07:36 +0530

    Changed the PORT Number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT =4000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
